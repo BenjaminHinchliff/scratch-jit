7602b2a6ed273bf819cd2abcee964e4313039d1c
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-09-03 15:56:04 -0700

    split renderer to separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,18 @@
+mod interpreter;
+mod renderer;
 mod unpack;
 
-use std::{env, fs};
+use std::fs;
 
-use bevy::{
-    core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode,
-    window::PresentMode,
-};
+use bevy::{prelude::*, window::PresentMode};
 use clap::Parser;
+use renderer::Renderer;
 use scratch_edu_parser::Project;
 use temp_dir::TempDir;
 
-fn setup_scene(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle {
-        projection: OrthographicProjection {
-            left: -240.0,
-            right: 240.0,
-            top: 180.0,
-            bottom: -180.0,
-            scaling_mode: ScalingMode::Auto {
-                min_width: 480.0,
-                min_height: 360.0,
-            },
-            ..default()
-        },
-        camera_2d: Camera2d {
-            clear_color: ClearColorConfig::Custom(Color::WHITE),
-        },
-        ..default()
-    });
-}
-
-struct ScratchProject {
-    project: Project,
-    assets: TempDir,
-}
-
-fn deg_to_rad(deg: f32) -> f32 {
-    deg * std::f32::consts::PI / 180.0
-}
-
-fn load_scratch(
-    mut commands: Commands,
-    scratch: Res<ScratchProject>,
-    asset_server: Res<AssetServer>,
-) {
-    for (i, target) in scratch.project.targets.iter().enumerate() {
-        let costume = &target.costumes[target.current_costume];
-        commands.spawn_bundle(SpriteBundle {
-            texture: asset_server.load(
-                scratch
-                    .assets
-                    .path()
-                    .join(&target.costumes[target.current_costume].md5ext),
-            ),
-            transform: Transform {
-                translation: Vec3::new(target.x.unwrap_or(0.0), target.y.unwrap_or(0.0), i as f32),
-                rotation: Quat::from_axis_angle(
-                    Vec3::Z,
-                    deg_to_rad(target.direction.map(|t| t - 90.0).unwrap_or(0.0)),
-                ),
-                scale: Vec2::splat(
-                    1.0 / costume.bitmap_resolution as f32
-                        * (target.size.map(|t| t / 100.0).unwrap_or(1.0)),
-                )
-                .extend(1.0),
-            },
-            ..default()
-        });
-    }
+pub struct ScratchProject {
+    pub project: Project,
+    pub assets: TempDir,
 }
 
 #[derive(Debug, Parser)]
@@ -95,7 +40,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_system(bevy::window::close_on_esc)
         .insert_resource(ScratchProject { project, assets })
-        .add_startup_system(setup_scene)
-        .add_startup_system(load_scratch)
+        .add_plugin(Renderer)
         .run();
 }
---
+++ src/renderer.rs
@@ -0,0 +1,66 @@
+use crate::ScratchProject;
+use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode};
+
+fn setup_scene(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle {
+        projection: OrthographicProjection {
+            left: -240.0,
+            right: 240.0,
+            top: 180.0,
+            bottom: -180.0,
+            scaling_mode: ScalingMode::Auto {
+                min_width: 480.0,
+                min_height: 360.0,
+            },
+            ..default()
+        },
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::Custom(Color::WHITE),
+        },
+        ..default()
+    });
+}
+
+fn deg_to_rad(deg: f32) -> f32 {
+    deg * std::f32::consts::PI / 180.0
+}
+
+fn load_scratch(
+    mut commands: Commands,
+    scratch: Res<ScratchProject>,
+    asset_server: Res<AssetServer>,
+) {
+    for (i, target) in scratch.project.targets.iter().enumerate() {
+        let costume = &target.costumes[target.current_costume];
+        commands.spawn_bundle(SpriteBundle {
+            texture: asset_server.load(
+                scratch
+                    .assets
+                    .path()
+                    .join(&target.costumes[target.current_costume].md5ext),
+            ),
+            transform: Transform {
+                translation: Vec3::new(target.x.unwrap_or(0.0), target.y.unwrap_or(0.0), i as f32),
+                rotation: Quat::from_axis_angle(
+                    Vec3::Z,
+                    deg_to_rad(target.direction.map(|t| t - 90.0).unwrap_or(0.0)),
+                ),
+                scale: Vec2::splat(
+                    1.0 / costume.bitmap_resolution as f32
+                        * (target.size.map(|t| t / 100.0).unwrap_or(1.0)),
+                )
+                .extend(1.0),
+            },
+            ..default()
+        });
+    }
+}
+
+pub struct Renderer;
+
+impl Plugin for Renderer {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_scene)
+            .add_startup_system(load_scratch);
+    }
+}
