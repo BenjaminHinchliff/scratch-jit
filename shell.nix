0870903ab7b1aed1ea6509ee5270fb271587db35
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-09-04 14:31:23 -0700

    add additional test data

--- shell.nix
+++ shell.nix
@@ -11,6 +11,6 @@ with pkgs; mkShell rec {
     xlibsWrapper xorg.libXcursor xorg.libXrandr xorg.libXi # To use x11 feature
     libxkbcommon wayland # To use wayland feature
   ];
-  RUSTC_VERSION = 1.63;
+  RUSTC_VERSION = "stable";
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
 }
